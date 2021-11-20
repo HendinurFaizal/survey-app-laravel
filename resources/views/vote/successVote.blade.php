90b503e8466ced0813dfc0570424fd311455ab78
mhnaufal <aal.pirates16@gmail.com>
2021-11-20 14:42:32 +0700

    create vote success page

---
+++ resources/views/vote/successVote.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Vote | COBLOS</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta content="PPL, Form, tugas, praktikum, proyek perangkat lunak, informatika, undip, universitas diponegoro"
+        name="keywords" />
+    <meta content="Tugas Mini Project PPL Kelas A" name="description" />
+    <meta property="og:title" content="Kelas A" />
+    <meta property="og:description" content="Tugas Mini Project PPL Kelas A" />
+    <meta name="title" content="Kelas A" />
+    <meta name="description" content="Tugas Mini Project PPL Kelas A" />
+
+    <!-- Bootstrap CSS -->
+
+</head>
+
+<body>
+    Thank you for your vote!
+</body>
+
+</html>
