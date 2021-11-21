74216e6f79578d2b6ffafb267ad01f38842a6889
mhnaufal <aal.pirates16@gmail.com>
2021-11-21 14:23:19 +0700

    buat user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $user1 = new User([
+            'name' => 'Bernald',
+            'email' => 'bernald@unchdeep.ac.id',
+            'password' => Hash::make('12341234'),
+        ]);
+        $user1->save();
     }
 }
