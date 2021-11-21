ba348bb138ceff259f3ba5080f7adf0c9c5613da
mhnaufal <aal.pirates16@gmail.com>
2021-11-21 14:19:40 +0700

    edit controller dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function showDashboard(Request $request)
     {
-        return view('dashboard');
+        if (Auth::check()) {
+            $user = Auth::user()->name;
+            return view('dashboard', compact('user'));
+        } else {
+            return redirect('login')->with('error', '❌ Anda belum login!');
+        }
     }
 }
